d3bd7dfe0343e213770751234807c050f7b0e0ba
mdos-san <mdossan@protonmail.com>
2021-08-06 18:50:46 +0200

    ♻️ Refact logic into separate function

--- index.js
+++ index.js
@@ -4,22 +4,29 @@ const SYMBOL = "BTCUSDT";
 const INTERVAL = "1m";
 
 const https = require("https");
-const url = new URL(PATH, BASE_URL);
-url.searchParams.append("symbol", SYMBOL);
-url.searchParams.append("interval", INTERVAL);
+const requestBinanceFactory = (baseUrl, path, symbol, interval) => () => {
+  return new Promise((resolve, reject) => {
+    const url = new URL(path, baseUrl);
+    url.searchParams.append("symbol", symbol);
+    url.searchParams.append("interval", interval);
 
-const request = https.get(url, res => {
-  let raw = "";
+    const request = https.get(url, res => {
+      let raw = "";
 
-  res.on('data', d => {
-    raw += d;
+      res.on('data', d => {
+	raw += d;
+      });
+
+      res.on('end', () => {
+	resolve(JSON.parse(raw));
+      });
+    });
+
+    request.on("error", reject);
+    request.end();
   });
+}
+const requestBinance = requestBinanceFactory(BASE_URL, PATH, SYMBOL, INTERVAL);
 
-  res.on('end', () => {
-    console.log(raw);
-  });
-});
-
-request.on("error", console.error);
-request.end();
+requestBinance().then(console.log).catch(console.error);
 
