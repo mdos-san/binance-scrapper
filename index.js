05f356927e3c8fbfb39f470a042cca7fc3797bde
mdos-san <mdossan@protonmail.com>
2021-08-06 18:42:35 +0200

    🚧 Make basic request to binance

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const BASE_URL = "https://api.binance.com";
 const PATH = "/api/v3/klines";
-const SYMBOL = "BTCUSD";
+const SYMBOL = "BTCUSDT";
 const INTERVAL = "1m";
+
+const https = require("https");
+const url = new URL(PATH, BASE_URL);
+url.searchParams.append("symbol", SYMBOL);
+url.searchParams.append("interval", INTERVAL);
+
+const request = https.get(url, res => {
+  let raw = "";
+
+  res.on('data', d => {
+    raw += d;
+  });
+
+  res.on('end', () => {
+    console.log(raw);
+  });
+});
+
+request.on("error", console.error);
+request.end();
 
