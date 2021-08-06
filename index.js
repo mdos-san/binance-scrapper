30f6cf1808f299ad45922f46be46d35b91083dc2
mdos-san <mdossan@protonmail.com>
2021-08-06 19:30:33 +0200

    ✨ Create scrapper loop and print to stdout

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const BASE_URL = "https://api.binance.com";
 const PATH = "/api/v3/klines";
 const SYMBOL = "BTCUSDT";
 const INTERVAL = "1m";
+const LIMIT = "2";
 
 const https = require("https");
-const requestBinanceFactory = (baseUrl, path, symbol, interval) => () => {
+const { setInterval } = require("timers");
+const requestBinanceFactory = (baseUrl, path, symbol, interval) => (endTime) => {
   return new Promise((resolve, reject) => {
     const url = new URL(path, baseUrl);
     url.searchParams.append("symbol", symbol);
     url.searchParams.append("interval", interval);
+    url.searchParams.append("limit", LIMIT);
+    if (endTime) {
+      url.searchParams.append("endTime", endTime);
+    }
 
     const request = https.get(url, res => {
       let raw = "";
@@ -28,5 +34,27 @@ const requestBinanceFactory = (baseUrl, path, symbol, interval) => () => {
 }
 const requestBinance = requestBinanceFactory(BASE_URL, PATH, SYMBOL, INTERVAL);
 
-requestBinance().then(console.log).catch(console.error);
+// Scrapper
+let endTime = 0;
 
+const main = async () => {
+  const data = await requestBinance();
+  endTime = data[0][0];
+  print(data.reverse());
+  loop();
+}
+main();
+
+const loop = async () => {
+  const data = await requestBinance(endTime);
+  data.pop(); // last one correspond to the endtime that is already printed
+  endTime = data[0][0];
+  print(data.reverse());
+  setTimeout(loop, 1000);
+}
+
+const print = (arr) => {
+  arr.forEach((e) => {
+    process.stdout.write(JSON.stringify(e) + "\n");
+  });
+}
