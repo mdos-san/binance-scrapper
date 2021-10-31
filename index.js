d3a079dd04ad98587efce2a9119982c774b9a045
mdos-san <mdossan@protonmail.com>
2021-10-31 02:45:52 +0100

    ♻️ Split requestBinance into another file

--- index.js
+++ index.js
@@ -2,37 +2,8 @@ const BASE_URL = "https://api.binance.com";
 const PATH = "/api/v3/klines";
 const SYMBOL = "BTCUSDT";
 const INTERVAL = "1m";
-const LIMIT = "2";
 
-const https = require("https");
-const { setInterval } = require("timers");
-const requestBinanceFactory = (baseUrl, path, symbol, interval) => (endTime) => {
-  return new Promise((resolve, reject) => {
-    const url = new URL(path, baseUrl);
-    url.searchParams.append("symbol", symbol);
-    url.searchParams.append("interval", interval);
-    url.searchParams.append("limit", LIMIT);
-    if (endTime) {
-      url.searchParams.append("endTime", endTime);
-    }
-
-    const request = https.get(url, res => {
-      let raw = "";
-
-      res.on('data', d => {
-	raw += d;
-      });
-
-      res.on('end', () => {
-	resolve(JSON.parse(raw));
-      });
-    });
-
-    request.on("error", reject);
-    request.end();
-  });
-}
-const requestBinance = requestBinanceFactory(BASE_URL, PATH, SYMBOL, INTERVAL);
+const requestBinance = require("./request-binance")(BASE_URL, PATH, SYMBOL, INTERVAL);
 
 // Scrapper
 let endTime = 0;
---
+++ request-binance.js
@@ -0,0 +1,32 @@
+const LIMIT = "2";
+
+const https = require("https");
+
+const requestBinanceFactory = (baseUrl, path, symbol, interval) => (endTime) => {
+  return new Promise((resolve, reject) => {
+    const url = new URL(path, baseUrl);
+    url.searchParams.append("symbol", symbol);
+    url.searchParams.append("interval", interval);
+    url.searchParams.append("limit", LIMIT);
+    if (endTime) {
+      url.searchParams.append("endTime", endTime);
+    }
+
+    const request = https.get(url, res => {
+      let raw = "";
+
+      res.on('data', d => {
+	raw += d;
+      });
+
+      res.on('end', () => {
+	resolve(JSON.parse(raw));
+      });
+    });
+
+    request.on("error", reject);
+    request.end();
+  });
+}
+
+module.exports = requestBinanceFactory;
